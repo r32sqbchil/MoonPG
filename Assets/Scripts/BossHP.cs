89bcc148d2254a316d235b58eb7a973b9bd21a39
r32sqbchil <r32sqbchil@g.shingu.ac.kr>
2021-11-28 21:17:02 +0900

    보스 HP와 HP바를 연결

--- Assets/Scripts/BossHP.cs
+++ Assets/Scripts/BossHP.cs
@@ -3,38 +3,34 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
+
 public class BossHP : MonoBehaviour
 {
     public float maxHealth = 1000;
-    float health;
-    // public Enemy Health;
 
     public Image healthBar;
     Animator anim;
+    private Enemy enemy;
 
     void Awake()
     {
         anim = GetComponent<Animator>();
-        health = maxHealth;
+        enemy = GetComponent<Enemy>();
+        enemy.SetHealth(maxHealth);
     }
 
     public void Update()
     {
-        if(Input.GetKey(KeyCode.X))
-        {
-            health -= 1;
-        }
-
-        healthBar.fillAmount =  health / maxHealth;
+        healthBar.fillAmount =  enemy.GetHealth() / maxHealth;
     }
 
     void OnTriggerEnter2D(Collider2D other)
     {
-        if(other.tag == "Player")
-            if(health <= 0)
-            {
-                anim.Play("BossDie");
-                Destroy(this.gameObject, 2f);
-            }
+        // if(other.tag == "Player")
+        //     if(bossStat.Health <= 0)
+        //     {
+        //         anim.Play("BossDie");
+        //         Destroy(this.gameObject, 2f);
+        //     }
     }
 }
--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 //구조체 Enemystat을 만듭니다. 안에는 Health나 Speed등을 넣습니다. 구조체를 inspector창에 보일수 있게 Attribute[System.Serializable]을 써줍니다.
 [System.Serializable]
 struct Enemystat{
-    public float Health;
+    public float health;
 }
 
 
@@ -19,14 +19,23 @@ public class Enemy : MonoBehaviour
         rigid = GetComponent<Rigidbody2D>();
     }
 
+    public void SetHealth(float health)
+    {
+        enemystat.health = health;
+    }
+
+    public float GetHealth()
+    {
+        return enemystat.health;
+    }
     //데미지를 받는 함수 입니다. 인자에는 damage값을 설정해줍니다.
     public void TakeDamage(float direction, float damage)
     {
         //체력이 damage만큼 까지게 합니다.
-        enemystat.Health -= damage;
-        Debug.Log("Enemy-HP: "+ enemystat.Health);
+        enemystat.health -= damage;
+        Debug.Log("Enemy-HP: "+ enemystat.health);
         //체력이 0이하로 내려가면 게임 오브젝트를 파괴합니다.
-        if(enemystat.Health <= 0)
+        if(enemystat.health <= 0)
         {
             Destroy(gameObject);
         } else {
