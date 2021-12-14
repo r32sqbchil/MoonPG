73eff84bb37e32ff5c5ecaa6a9e8b4ce3df4074d
jooken <jooken@naver.com>
2021-12-15 00:40:12 +0900

    씬 상수값 정리

---
+++ Assets/Scripts/Scene.cs
@@ -0,0 +1,8 @@
+
+public class Scene
+{
+    public const string SCENE_TOWN_STAGE  = "TownStage";
+    public const string SCENE_TOWN_STAGE1 = "TownStage1";
+    public const string SCENE_TOWN_STAGE2 = "TownStage2";
+    public const string SCENE_TOWN_STAGE3 = "TownStage3";
+}
