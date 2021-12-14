41cdf497d4be9039f41ef83e9fb3446d37f7b1a1
jooken <jooken@naver.com>
2021-12-15 00:40:36 +0900

    적 공통 스크립트 신규

---
+++ Assets/Scripts/Enemy & Boss/EnemyBase.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//구조체 Enemystat을 만듭니다. 안에는 Health나 Speed등을 넣습니다. 구조체를 inspector창에 보일수 있게 Attribute[System.Serializable]을 써줍니다.
+[System.Serializable]
+struct EnemyStat{
+    public float health;
+}
+
+public class EnemyBase : MonoBehaviour
+{
+    [SerializeField]private EnemyStat enemyStat;
+
+    private GameManager gameManager;
+    private CameraShake cameraShake;
+
+    void Start()
+    {
+        gameManager = GameObject.FindObjectOfType<GameManager>();
+        cameraShake = GameObject.FindObjectOfType<CameraShake>();
+    }
+
+    void Update()
+    {
+        
+    }
+
+    //데미지를 받는 함수 입니다. 인자에는 damage값을 설정해줍니다.
+    public void TakeDamage(GameObject playerObject, float direction, float damage)
+    {
+        //체력이 damage만큼 까지게 합니다.
+        enemyStat.health -= damage;
+
+        Debug.Log("Enemy-HP: "+ enemyStat.health);
+
+        if(enemyStat.health <= 0) {
+            gameManager.Action(gameObject, GameManager.ACTION_ON_DIED);
+            Destroy(gameObject, 2f);
+        } else {
+            StartCoroutine(cameraShake.ShakeHorizontalOnly(.1f, .1f));
+        }
+    }
+}
