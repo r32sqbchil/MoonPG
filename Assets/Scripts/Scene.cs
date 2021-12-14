3270a16338bb2a198b3caafac17abba22b1e60ed
r32sqbchil <r32sqbchil@g.shingu.ac.kr>
2021-12-15 01:00:14 +0900

    씬 스크립트 확장

--- Assets/Scripts/Scene.cs
+++ Assets/Scripts/Scene.cs
@@ -2,8 +2,16 @@
 public class Scene
 {
     public const string SCENE_TOWN_STAGE  = "TownStage";
+    public const string SCENE_TOWN_SUBSTAGE  = "TownSubstage";
     public const string SCENE_TOWN_STAGE1 = "TownStage1";
     public const string SCENE_TOWN_STAGE2 = "TownStage2";
     public const string SCENE_TOWN_STAGE3 = "TownStage3";
     public const string SCENE_MOUNTAIN_STAGE = "MountainStage";
+    public const string SCENE_MOUNTAIN_SUBSTAGE = "MountainSubstage";
+    public const string SCENE_MOUNTAIN_STAGE1 = "MountainStage1";
+    public const string SCENE_MOUNTAIN_STAGE2 = "MountainStage2";
+    public const string SCENE_MOUNTAIN_STAGE3 = "MountainStage3";
+    public const string SCENE_MOUNTAIN_STAGE4 = "MountainStage4";
+    public const string SCENE_MOUNTAIN_STAGE5 = "MountainStage5";
+    public const string SCENE_MOUNTAIN_STAGE6 = "MountainStage6";
 }
