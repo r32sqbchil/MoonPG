e890a5a9309042aa8cfd626f3e318404b75bc476
jooken <jooken@naver.com>
2021-12-17 00:02:07 +0900

    마운틴2 퀘스트4 추가

--- Assets/Scripts/Quest/QuestManager.cs
+++ Assets/Scripts/Quest/QuestManager.cs
@@ -34,7 +34,7 @@ public class QuestManager : MonoBehaviour
         questHandlerMap.Add(Scene.SCENE_TOWN_STAGE3+"$400", new TownStage3Quest4Handler());
         questHandlerMap.Add(Scene.SCENE_MOUNTAIN_STAGE+"$200", new MountainStageQuest2Handler());
         questHandlerMap.Add(Scene.SCENE_MOUNTAIN_STAGE+"$400", new MountainStageQuest4Handler());
-        //questHandlerMap.Add(Scene.SCENE_MOUNTAIN_STAGE2+"$400", new MountainStage2Quest4Handler());
+        questHandlerMap.Add(Scene.SCENE_MOUNTAIN_STAGE2+"$400", new MountainStage2Quest4Handler());
         //questHandlerMap.Add(Scene.SCENE_MOUNTAIN_STAGE2+"$400", new MountainStage2Quest4Handler());
         //questHandlerMap.Add(Scene.SCENE_MOUNTAIN_STAGE4+"$400", new MountainStage4Quest4Handler());
         //questHandlerMap.Add(Scene.SCENE_MOUNTAIN_STAGE5+"$400", new MountainStage5Quest4Handler());
