9442da78967469a724d1724771a58943beb75650
jooken <jooken@naver.com>
2021-12-17 01:04:35 +0900

    마운틴5 클리어

--- Assets/Scripts/Quest/Mountain/MountainStage5Quest4Handler.cs
+++ Assets/Scripts/Quest/Mountain/MountainStage5Quest4Handler.cs
@@ -1,18 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
-public class MountainStage5Quest4Handler : MonoBehaviour
+public class MountainStage5Quest4Handler : QuestHandler
 {
-    // Start is called before the first frame update
-    void Start()
+    public override void OnAction(string actionName, Dictionary<string, object> context)
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        if(actionName == QuestHandler.EVENT_END_OF_TALK)
+        {
+            int step = GetQuestStep(context);
+            if(step == 0) {
+                SetQuestStep(context, 10);
+                SceneManager.LoadScene(SCENE_MOUNTAIN_STAGE6);
+            }
+        }
     }
 }
