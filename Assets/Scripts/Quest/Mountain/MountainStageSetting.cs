b5821b5f8d348a8a3286c40d931a2a68b23208dd
jooken <jooken@naver.com>
2021-12-19 01:03:52 +0900

    포털 타고 되돌아 올 장소 지정

--- Assets/Scripts/Quest/Mountain/MountainStageSetting.cs
+++ Assets/Scripts/Quest/Mountain/MountainStageSetting.cs
@@ -12,7 +12,7 @@ public class MountainStageSetting : MonoBehaviour
             QuestHandler.SetContextValue(context, KEY_PORTAL_HIDE, true);
         }
     }
-    // Start is called before the first frame update
+
     void Start()
     {
         QuestManager questManager = GameManager.FindQuestManager();
@@ -33,6 +33,17 @@ public class MountainStageSetting : MonoBehaviour
             Portal portal = GameObject.FindObjectOfType<Portal>();
             if(portal != null) {
                 portal.gameObject.SetActive(false);
+            }
+        } else {
+            Player player = GameObject.FindObjectOfType<Player>();
+            GameObject returnPosition = GameObject.Find("ReturnPosition");
+            if(player && returnPosition){
+                Vector3 playerPosition = player.transform.position;
+                playerPosition.x = returnPosition.transform.position.x;
+                playerPosition.y = returnPosition.transform.position.y;
+                player.transform.position = playerPosition;
+            } else {
+                Debug.LogWarning("Can't find Player or ReturnPosition");
             }
         }
     }
