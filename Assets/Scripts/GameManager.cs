5aaf43a951591a221e9640ea235cb7da94b339bc
jooken <jooken@naver.com>
2021-12-19 14:30:58 +0900

    게임매니저에서 몬스터 스폰관련 기능 폐기

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -11,11 +11,6 @@ public class GameManager : MonoBehaviour
     public const string ACTION_ON_TOUCH_LEFT_BOUNDARY = "OnTouchLeftBoundary";
     public const string ACTION_ON_TOUCH_RIGHT_BOUNDARY = "OnTouchRightBoundary";
 
-    public Transform[] points;
-    public GameObject[] monsterPrefab;
-
-    public float createTime;
-    public int maxMonster = 1;
 
     public GameObject subSettings;
     public GameObject Inspector;
@@ -166,21 +161,7 @@ public class GameManager : MonoBehaviour
 
         SettingForMovingLimitX();
 
-        GameObject spawnPoint = GameObject.Find("SpawnPoint");
-
         fade.FadeOut();
-        
-        if(spawnPoint != null)
-        {
-            //Hierarchy View의 Spawn Point를 찾아 하위에 있는 모든 Transform 컴포넌트를 찾아옴
-            points = spawnPoint.GetComponentsInChildren<Transform>();
-    
-            if(points.Length > 0)
-            {
-                //몬스터 생성 코루틴 함수 호출
-                StartCoroutine(this.CreateMonster());
-            }
-        }
     }
 
     void Update()
@@ -201,30 +182,7 @@ public class GameManager : MonoBehaviour
         }
     }
 
-    IEnumerator CreateMonster()
-    {
-        //게임 종료 시까지 무한 루프
-        while( !isGameOver )
-        {
-            //현재 생성된 몬스터 개수 산출
-            int monsterCount = (int)GameObject.FindGameObjectsWithTag("Enemy").Length;
- 
-            if(monsterCount < maxMonster)
-            {
-                //몬스터의 생성 주기 시간만큼 대기
-                yield return new WaitForSeconds(createTime);
- 
-                //불규칙적인 위치 산출
-                int idx = Random.Range(1, points.Length);
-                //몬스터의 동적 생성
-                int monsterIndex = Random.Range(0,monsterPrefab.Length);
-                Instantiate(monsterPrefab[monsterIndex], points[idx].position, points[idx].rotation);
-            }else
-            {
-                yield return null;
-            }
-        }
-    }
+
 
     void SettingForMovingLimitX(){
         SetLimitMoveXMin("MovingAreaX1");
